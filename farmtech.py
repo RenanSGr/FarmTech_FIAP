65008d2e84f994469dacf8e91aebdbf49f2a77c2
RenanSGr <302555582+RenanSGr@users.noreply.github.com>
2026-09-16 22:32:25 -0300

    Função de excluir fazenda

--- farmtech.py
+++ farmtech.py
@@ -79,6 +79,18 @@ def incluir_fazenda():
         salvar_fazenda(fazenda,cultura,area_m2,tipo_insumo,quantidade_insumo,unidade_insumo)
         break
 
+def excluir_fazenda():
+    listar_fazendas()
+    while True:
+        if registros:
+            id_fazenda = int(input("\nDigite o ID da fazenda que deseja excluir: "))
+            if 0 <= id_fazenda < len(registros):
+                fazenda_removida = registros.pop(id_fazenda)
+                print(f"Fazenda '{fazenda_removida['fazenda']}' excluída com sucesso!")
+                break
+            else:
+                print("ID não encontrado.")
+
 def menu():
     while True:
         print("\n### FarmTech Solutions ###")
