088fef1ebc5d0d12c4a81f43135649783d08c631
RenanSGr <302555582+RenanSGr@users.noreply.github.com>
2026-09-16 21:53:03 -0300

    Inclusão cultura Cana-de-açucar
    - Lógica de inclusão
    
    - Adição da primeira cultura

--- farmtech.py
+++ farmtech.py
@@ -1,3 +1,35 @@
+registros = []
+
+def salvar_fazenda(nome_fazenda, cultura, area_m2, tipo_insumo, quantidade_insumo, unidade_insumo):
+    registro_novo = {"fazenda": nome_fazenda,"cultura": cultura,"area_m2": area_m2, "tipo_insumo": tipo_insumo, "quantidade_insumo": quantidade_insumo, "unidade_insumo": unidade_insumo}
+    registros.append(registro_novo)
+    print(f"Nova fazenda registrada: {registro_novo}")
+
+def calcular_area_retangular(largura, comprimento):
+    return largura * comprimento
+
+def incluir_fazenda():
+    while True:
+        print("1. Cana-de-açúcar")
+
+        cultura = input("Insira a cultura: ")
+
+        if cultura == "1":
+            fazenda = input("\nInsira o nome da fazenda: ")
+            cultura = "Cana-de-açúcar"
+            largura = float(input("Insira a largura do terreno em metros: "))
+            comprimento = float(input("Insira a comprimento do terreno em metros: "))
+            area_m2 = calcular_area_retangular(largura, comprimento)
+            tipo_insumo = "Fertilizante NPK"
+            unidade_insumo = "kg"
+            quantidade_insumo_kg = 500 * (area_m2 / 10000)
+        else:
+            print("Opção Inválida!\n")
+            continue
+
+        salvar_fazenda(fazenda,cultura,area_m2,tipo_insumo,unidade_insumo,quantidade_insumo_kg)
+        break
+
 def menu():
     while True:
         print("\n### FarmTech Solutions ###")
@@ -11,7 +43,7 @@ def menu():
         print("")
 
         if opcao == "1":
-            print("Entrada de dados")
+            incluir_fazenda()
         elif opcao == "2":
             print("Saída de dados")
         elif opcao == "3":
